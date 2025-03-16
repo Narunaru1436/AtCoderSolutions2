63c7480700a2b236d84531730322be68c24f1eae
Narunaru1436 <narunaru1546@gmail.com>
2025-03-16 22:17:29 +0900

    問題を解く方針はわかったけど尺取り法が理解できない

--- abc388/c/main.cpp
+++ abc388/c/main.cpp
@@ -1,7 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-    // 問題のコードをここに書く
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    for (auto&& a : A)
+        cin >> a;
+
+    long ans = 0;
+    // a / 2 より大きい最初の要素（なければ最後の次）を表す値 j
+    for (int j = 0; const auto a : A) {
+        // 越えるまで進める
+        while (j < N && A[j] * 2 <= a) j++;
+        ans += j;
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
