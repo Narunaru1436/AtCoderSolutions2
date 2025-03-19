780f0e1bca866f42193987e81f222461cc4dc7fd
Narunaru1436 <narunaru1546@gmail.com>
2025-03-19 23:40:14 +0900

    とりあえずコミット

--- abc393/a/main.cpp
+++ abc393/a/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    // 問題のコードをここに書く
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int ans = 4;
+    if (s1 == "sick" && s2 == "sick") ans = 1;
+    else if (s1 == "sick" && s2 == "fine") ans = 2;
+    else if (s1 == "fine" && s2 == "sick") ans = 3;
+
+    cout << ans  << endl;
     return 0;
 }
