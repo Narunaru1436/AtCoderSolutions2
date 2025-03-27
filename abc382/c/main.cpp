a5aad2e949d233b1885c7e1935a04912a466d25e
Narunaru1436 <narunaru1546@gmail.com>
2025-03-27 23:41:22 +0900

    二分探索少し

--- abc382/c/main.cpp
+++ abc382/c/main.cpp
@@ -7,32 +7,22 @@ using namespace std;
 int main() {
     int N, M;
     cin >> N >> M;
-    
-    vector<pair<int, int>> people(N);
-    for (int i = 0; i < N; i++) {
-        cin >> people[i].first;
-        people[i].second = i + 1; // 人の番号を記録
+
+    vector<int> A(N);
+    vector<int> B(M);
+
+    for (auto& a : A) cin >> a;
+    for (auto& b : B) cin >> b;
+
+    int i;
+    for (i = 1; i < N; i++) {
+        A[i] = min(A[i], A[i - 1]);
     }
 
-    vector<int> sushi(M);
-    for (int i = 0; i < M; i++) {
-        cin >> sushi[i];
-    }
-
-    // 美食度の小さい順にソート
-    sort(people.begin(), people.end());
-
-    int index = 0; // 次にチェックする人のインデックス
-    for (int j = 0; j < M; j++) {
-        while (index < N && people[index].first < sushi[j]) {
-            index++; // 食べられる人を探す
-        }
-        if (index < N) {
-            cout << people[index].second << endl;
-            index++; // この人はもう寿司を取ったのでスキップ
-        } else {
-            cout << -1 << endl;
-        }
+    for (i = 0; i < M; i++) {
+        int pos = lower_bound(A.begin(), A.end(), B[i], greater<int>()) - A.begin();
+        if (pos == N) cout << -1 << endl;
+        else cout << pos + 1 << endl;
     }
 
     return 0;
