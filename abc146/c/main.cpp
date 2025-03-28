f4b6a61fc35d87a24ef25934a986b295fb93cb84
Narunaru1436 <narunaru1546@gmail.com>
2025-03-28 23:49:08 +0900

    二分探索で欲しい値に最も近い値を得る

--- abc146/c/main.cpp
+++ abc146/c/main.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // 問題のコードをここに書く
+long long CalcKeta(long long c)
+{
+    int ret = 0;
+    while (c > 0) {
+        c /= 10;
+        ret++;
+    }
+
+    return ret;
+}
+
+int main() 
+{
+    long long A, B, X;
+    cin >> A >> B >> X;
+
+    long long l = 0;
+    long long r = 1e9 + 1;
+    while (r - l > 1) {
+        long long c = (l + r) / 2;
+        if (A * c + B * CalcKeta(c) <= X) {
+            l = c;
+        }
+        else {
+            r = c;
+        }
+    }
+
+    cout << l << endl;
     return 0;
 }
