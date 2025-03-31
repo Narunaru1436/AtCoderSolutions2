27e08596731766b39b557918a83deb278b4f78c5
Narunaru1436 <narunaru1546@gmail.com>
2025-03-31 23:37:22 +0900

    二分探索

--- abc376/c/main.cpp
+++ abc376/c/main.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 int main() {
-    // 問題のコードをここに書く
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    vector<int> B(n - 1);
+    for (auto& a : A) cin >> a;
+    for (auto& b : B) cin >> b;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    auto judge = [&](int x) {
+        auto nb = B;
+        nb.push_back(x);
+        sort(nb.begin(), nb.end());
+        for (int i = 0; i < n; i++) if (A[i] > nb[i]) return false;
+        return true;
+    };
+
+    const int INF = 1e9;
+    int right = INF;
+    int left = 0;
+    while (right - left > 1) {
+        int mid = (left + right) / 2;
+        if (judge(mid)) right = mid;
+        else left = mid;
+    }
+
+    if (right == INF) cout << -1 << endl;
+    else cout << left << endl;
     return 0;
 }
